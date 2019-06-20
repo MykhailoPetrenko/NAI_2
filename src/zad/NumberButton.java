393d99e2f55ced45a7395a0f73d84873d7533558
Erok <50800579+MykhailoPetrenko@users.noreply.github.com>
2019-06-20 15:12:39 +0300

    Update NumberButton.java

--- src/zad/NumberButton.java
+++ src/zad/NumberButton.java
@@ -1,1 +1,14 @@
-package zad;import javax.swing.*;public class NumberButton extends JToggleButton {    int number = 0;    public int getNumber() {        return number;    }    public void setNumber(int number) {        this.number = number;    }}
+package zad;
+
+import javax.swing.*;
+
+public class NumberButton extends JToggleButton {
+    int number = 0; 
+
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
