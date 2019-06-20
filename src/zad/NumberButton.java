cadc13e63d0ed43022fc718cf6850914106b4328
Erok <fuligung@gmail.com>
2019-06-20 15:11:15 +0300

    commit

---
+++ src/zad/NumberButton.java
@@ -0,0 +1,1 @@
+package zad;import javax.swing.*;public class NumberButton extends JToggleButton {    int number = 0;    public int getNumber() {        return number;    }    public void setNumber(int number) {        this.number = number;    }}
